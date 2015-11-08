efca2d6d292da31d14973493f354234bb260d6ca
Diana Kornejeva <caron@inbox.lv>
2015-11-08 12:55:22 +0200

    Papildinata programma, tiek izskaitlots x un y reizinajums

--- 3uzd/3uzd/Program.cs
+++ 3uzd/3uzd/Program.cs
@@ -9,7 +9,7 @@ namespace _3uzd
     {
         static void Main(string[] args)
         {
-            int x, y;
+            int x, y, z;
 
             Console.WriteLine("Ievadiet x \n\t");
             x = int.Parse(Console.ReadLine());
@@ -20,6 +20,8 @@ namespace _3uzd
             Console.WriteLine("\n\tx = " + x.ToString()
                 + "\n\ty = " + y.ToString());
 
+            z = x * y;
+            Console.WriteLine("\n\tx * y = " + z.ToString());
             Console.ReadKey();
         }
     }
